b75f608d1f2a137da0ed75a696ee30a4fa976202
IsaiahWright902 <icwright902@gmail.com>
2023-03-29 16:41:13 -0400

    try this out

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: "/terry-loftus-landing-page/",
-  base: "/",
+  // base: "/",
   plugins: [react()],
 });
